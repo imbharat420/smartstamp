5e88c9126cb805b9b26426421bbd77bfd538cb94
imbharat420 <imbharat420@gmail.com>
2023-01-01 01:45:52 +0530

    response()->json()

--- routes/api.php
+++ routes/api.php
@@ -11,3 +11,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post("/login",[AuthController::class,"login"]);
 Route::post("/register",[AuthController::class,"register"]);
+Route::post("/logout",[AuthController::class,"logout"]);

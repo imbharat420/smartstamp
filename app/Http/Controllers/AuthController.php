e11eb84a922f9e708c333fb5383de90b78f63e8a
imbharat420 <imbharat420@gmail.com>
2023-01-04 02:37:39 +0530

    register API setup

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,21 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Http\Requests\StoreUserRequest;
+
 use Illuminate\Http\Request;
 use App\Traits\HttpResponses;
-use App\Http\Requests\StoreUserRequest;
+use Illuminate\Support\Facades\Hash;
+
 
 class AuthController extends Controller
 {
     use HttpResponses;
 
-    public function login(){
+    public function login(Request $request){
+        $request->validated($request->all());
+
         return response()->json("This is Login function");
     } 
     
     public function register(StoreUserRequest $request){
+        $request->validated($request->all());
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return $this->success([
+            "user" => $user,
+            "token" => $user->createToken('API TOKEN OF ' . $user->name)->plainTextToken
+        ]);
         
-        return response()->json("This is register function");
     }
 
     public function logout(){
--- app/Http/Requests/LoginUserRequest.php
+++ app/Http/Requests/LoginUserRequest.php
@@ -3,9 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rules;
 
-class StoreUserRequest extends FormRequest
+class LoginUserRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -16,7 +15,7 @@ class StoreUserRequest extends FormRequest
     {
         return true;
     }
- 
+
     /**
      * Get the validation rules that apply to the request.
      *
@@ -25,9 +24,7 @@ class StoreUserRequest extends FormRequest
     public function rules()
     {
         return [
-            "name"=>["required","string","max:255"],
-            "email"=>["required","string","email","max:255","unique:users"],
-            "password"=>["required","confirm",Rules\Password::defaults()],
+         
         ];
     }
 }

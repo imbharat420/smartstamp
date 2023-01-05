e0a687792f5782510e5408cd4ff23971d862a634
imbharat420 <imbharat420@gmail.com>
2023-01-06 01:47:14 +0530

    QrController filter on show

--- app/Http/Controllers/QRController.php
+++ app/Http/Controllers/QRController.php
@@ -63,9 +63,9 @@ class QrController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Qr $qr)
     {
-        return "show";
+        return new QrResource($qr);
     }
 
     /**
